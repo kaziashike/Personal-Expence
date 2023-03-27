2692048aa7be184fa1c7980118560eb51707eb73
Kazi Ashique Elahi <kaziashike@gmail.com>
2023-03-28 01:06:11 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Expences'),
+      ),
+      body: Column(
+        children: [
+          Card(
+            color: Colors.blue,
+            child: Container(width: double.infinity, child: Text('Chart')),
+          ),
+          Card(
+            child: Text('List of TX'),
+          )
+        ],
+      ),
+    );
+  }
+}
