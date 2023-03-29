0131c28d1f76493e354ed51db6e1d515b6ce658e
Kazi Ashique Elahi <kaziashike@gmail.com>
2023-03-30 05:26:20 +0600

    Changes to be committed: 	modified:   lib/transection.dart
    Changes not staged for commit:
    	modified:   lib/main.dart

--- lib/transection.dart
+++ lib/transection.dart
@@ -1,11 +1,22 @@
 class Transaction {
   String id;
   String title;
-  double cost;
+  int cost;
   DateTime date;
   Transaction(
       {required this.id,
       required this.title,
       required this.cost,
       required this.date});
+}
+
+List<Transaction> trans = [
+  Transaction(id: 't1', title: 'Shoe', cost: 1500, date: DateTime.now()),
+  Transaction(id: 't2', title: 'Grosery', cost: 3000, date: DateTime.now())
+];
+
+List history = [0, 0, 0, 0, 0, 0, 0];
+int returnTotal() {
+  trans.map((e) {if(e.date)});
+  return 0;
 }
